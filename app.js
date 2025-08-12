c3d089e47d35bc20ca8bdff3ca0abbe27c2fa2bd
2041002006pratikbal <2041002006.pratikbal@gmail.com>
2025-08-12 22:52:28 +0530

    nodejs aws

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+const path = require("path");
+
+app.use('/static',express.static(path.join(__dirname,'static files')));
+
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    console.log("This is landing page");
+    res.send("This is Landing page");
+});
+
+app.post("/about",(req,res)=>{
+    const {name} = req.body;
+    console.log("This is about page");
+    res.send(`This is About Page and name is ${name}`);
+});
+
+app.get("/home",(req,res)=>{
+    console.log("This is home page");
+    res.send("This is Home Page");
+});
+
+app.get("/contact",(req,res)=>{
+
+    res.send("This is contact us Page");
+});
+
+app.get("/image",(req,res)=>{
+    res.sendFile(path.join(__dirname,'image.jpg'));
+});
+
+app.get("/file",(req,res)=>{
+    res.sendFile(path.join(__dirname,'cv.pdf'));
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+});
+
+
